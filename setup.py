d94b9a1a7a8b36d3a36ae1866256f3e6fab9b748
NullpointerW <voidmanwzp@gmail.com>
2024-02-22 13:11:56 +0800

    commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bard-http-srv",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # 在这里列出你的库所需的其他Python包
+        'bardapi'
+        'Flask'
+    ],
+
+    author="vow1231a",
+    author_email="voidmanwzp@gmail.com",
+    description="bard http server",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/NullpointerW/bard-http-srv",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
