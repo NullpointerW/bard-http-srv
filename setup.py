c600d3dbb52878f643ffd22bcee940bd3357c288
NullpointerW <voidmanwzp@gmail.com>
2024-02-26 01:02:12 +0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bard-http-srv",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         # 在这里列出你的库所需的其他Python包
